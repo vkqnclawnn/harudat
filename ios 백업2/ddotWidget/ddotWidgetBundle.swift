3bf574729604dfff111129c8396afb4aaabb8fc8
vkqncalwnn <vkqncalwnn@gmail.com>
2026-01-18 14:36:16 +0900

    UPDATE

---
+++ ios 백업2/ddotWidget/ddotWidgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  ddotWidgetBundle.swift
+//  ddotWidget
+//
+//  Created by builder on 1/17/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ddotWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ddotWidget()
+        ddotWidgetControl()
+        ddotWidgetLiveActivity()
+    }
+}
